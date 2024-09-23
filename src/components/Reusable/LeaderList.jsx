529167a6228a1602cb229d7f53fc1142cc582921
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-09-23 23:54:20 +0530

    update

--- src/components/Reusable/LeaderList.jsx
+++ src/components/Reusable/LeaderList.jsx
@@ -1,11 +1,31 @@
+import PropTypes from "prop-types";
 import "../../styles/Reusable/leaderList.css";
 
-const LeaderList = ({bgc, color}) => {
+const LeaderList = ({ bgc, color }) => {
   return (
-    <div className="leaderListMain" style={{backgroundColor : bgc}}>
-      <div className="content" style={{backgroundColor : color}} ></div>
+    <div className="leaderListMain" style={{ backgroundColor: bgc }}>
+      <div className="content" style={{ backgroundColor: color }}>
+        <div className="content1">
+          <h3>Cody Fisher</h3>
+          <p className="none">CEO Founder</p>
+        </div>
+        <div className="content2">
+          <div className="icons">
+            <i className="fab fa-twitter"></i>
+          </div>
+          <div className="icons">  <i className="fab fa-instagram"></i>
+          </div>
+          <div className="icons">  <i className="fab fa-linkedin"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
+};
+
+LeaderList.propTypes = {
+  color: PropTypes.string.isRequired,
+  bgc: PropTypes.string.isRequired,
 };
 
 export default LeaderList;
