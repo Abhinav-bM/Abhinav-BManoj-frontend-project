83d5f6e633eb8a51d0f8107f8f5e97c14fbb502d
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-09-24 20:11:05 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,28 @@
-import "../styles/about.css"
+import "../styles/about.css";
 const About = () => {
   return (
     <section className="aboutSection">
-        <div className="aboutMain">
-            <p>
-            Who we are ?</p>
-            <div className="btn-area">
-            
-            </div>
-        </div>
+      <div className="aboutMain">
+        <p>          <div className="ellipse"></div>
+        Who we are ?</p>
+        <div className="btn-area"></div>
+      </div>
 
-        <div className="aboutContent">
-            <h3>We create mind-blowing visuals , brands <br />
-                websites and products <span>that help <br /> startups  
-                and innovative companies gain more exposure </span>
-            </h3>
-            <div className="about-btn">
-                <div>Learn More  </div>
-            </div>
+      <div className="aboutContent">
+        <h3>
+          We create mind-blowing visuals , brands <br />
+          websites and products{" "}
+          <span>
+            that help <br /> startups and innovative companies gain more
+            exposure{" "}
+          </span>
+        </h3>
+        <div className="about-btn">
+          <div>Learn More </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
-
+export default About;
