0ed9526d24eba0b755947279258078f6062833b3
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-09-24 20:21:16 +0530

    update

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -4,23 +4,28 @@ import ProjectList from "./Reusable/ProjectList";
 const ProjectSection = () => {
   return (
     <section className="projectSec">
-        <div className="projSec1">
-            <ProjectList />
-            <ProjectList />
+      <div className="projSec1">
+        <ProjectList />
+        <ProjectList />
+      </div>
+
+      <div className="projSec2">
+        <div className="projSec2First">
+          <div className="first">
+            {" "}
+            <div className="ellipse"></div>
+            Featured Works
+          </div>
+          <div className="second">
+            Take a look at <br /> our projects
+          </div>
         </div>
 
-        <div className="projSec2">
-
-            <div className="projSec2First">
-                <div className="first">Featured Works</div>
-                <div className="second">Take a look at <br /> our projects</div>
-            </div>
-            
-            <div className="projSec2Second">
-                <ProjectList />
-                <ProjectList />
-            </div>
-        </div>  
+        <div className="projSec2Second">
+          <ProjectList />
+          <ProjectList />
+        </div>
+      </div>
     </section>
   );
 };
