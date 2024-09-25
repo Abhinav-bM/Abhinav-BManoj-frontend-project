36658bf0bc332581f2dc0c0af5fb45de2398be47
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-09-26 00:20:57 +0530

    headerFix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import "../styles/components/header.css";
 
 const Header = () => {
@@ -17,21 +18,82 @@ const Header = () => {
         </div>
         <nav className="menu">
           <ul>
-            <li>Menu item</li>
-            <li>Menu item</li>
-            <li>Menu item</li>
-            <li>Menu item</li>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/service"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/service"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/service"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Contact
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
       <button className="cta1">Start a project</button>
 
-      <div className={`mobileNav ${menuActive ? 'menu-active' : ''}`}>
+      {/* Mobile Nav */}
+      <div className={`mobileNav ${menuActive ? "menu-active" : ""}`}>
         <ul>
-          <li>Menu item</li>
-          <li>Menu item</li>
-          <li>Menu item</li>
-          <li>Menu item</li>
+          <li>
+            <NavLink
+              to="/"
+              onClick={toggleMenu}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/service"
+              onClick={toggleMenu}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/service"
+              onClick={toggleMenu}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/service"
+              onClick={toggleMenu}
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Contact
+            </NavLink>
+          </li>
         </ul>
         <button className="cta2">Start a project</button>
       </div>
